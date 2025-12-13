2a6afaf44e3cab9589fa8fa9067b0e1954334a8e
Robert Escriva <robert@rescrv.net>
2025-12-13 08:07:00 -0800

    WIP

--- src/builtins/mod.rs
+++ src/builtins/mod.rs
@@ -8,6 +8,7 @@ mod echo;
 pub mod expand;
 mod fold;
 mod head;
+mod ln;
 mod ls;
 mod mkdir;
 mod nl;
@@ -47,6 +48,7 @@ where
         "expand" | "/usr/bin/expand" => Ok(expand::bin),
         "fold" | "/usr/bin/fold" => Ok(fold::bin),
         "head" | "/usr/bin/head" => Ok(head::bin),
+        "ln" | "/bin/ln" => Ok(ln::bin),
         "ls" | "/bin/ls" => Ok(ls::bin),
         "mkdir" | "/bin/mkdir" => Ok(mkdir::bin),
         "nl" | "/usr/bin/nl" => Ok(nl::bin),
