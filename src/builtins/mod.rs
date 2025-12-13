17c80269d64a0a749a8c8ed15166eb038e6aed7a
Robert Escriva <robert@rescrv.net>
2025-12-13 06:57:58 -0800

    WIP

--- src/builtins/mod.rs
+++ src/builtins/mod.rs
@@ -10,6 +10,7 @@ mod fold;
 mod head;
 mod nl;
 mod paste;
+mod pwd;
 mod seq;
 pub mod sh;
 mod sort;
@@ -43,6 +44,7 @@ where
         "head" | "/usr/bin/head" => Ok(head::bin),
         "nl" | "/usr/bin/nl" => Ok(nl::bin),
         "paste" | "/usr/bin/paste" => Ok(paste::bin),
+        "pwd" | "/bin/pwd" => Ok(pwd::bin),
         "seq" | "/usr/bin/seq" => Ok(seq::bin),
         "sort" | "/usr/bin/sort" => Ok(sort::bin),
         "tail" | "/usr/bin/tail" => Ok(tail::bin),
